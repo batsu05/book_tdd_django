b221f360b2f77e9328a7f9e5001ea31f7da0be21
Youngho Jeong <batsu05@gmail.com>
2015-06-02 09:22:47 +0900

    refactoring FT

--- functional_test.py
+++ functional_test.py
@@ -11,6 +11,11 @@ class NewVisitorTest(unittest.TestCase):
 	def tearDown(self):
 		self.browser.quit()
 
+	def check_row_list_table(self, row_text):
+		table = self.browser.find_element_by_id('id_list_table')
+		rows = table.find_elements_by_tag_name('tr')
+		self.assertIn(row_text, [row.text for row in rows])
+
 	def test_can_start_a_list_and_retrieve_it_later(self):
 		self.browser.get('http://localhost:8000')
 
@@ -18,12 +23,14 @@ class NewVisitorTest(unittest.TestCase):
 
 		header_text = self.browser.find_element_by_tag_name('h1').text
 		self.assertIn('To-Do',header_text)
-
+		
 		inputbox = self.browser.find_element_by_id('id_new_item')
 		self.assertEqual(inputbox.get_attribute('placeholder'), 'input item')
 
 		inputbox.send_keys('buy feather')
 		inputbox.send_keys(Keys.ENTER)
+
+		'''
 
 		# import time
 		# time.sleep(10)
@@ -45,8 +52,18 @@ class NewVisitorTest(unittest.TestCase):
 
 		self.assertIn('1: buy feather', [row.text for row in rows])
 		self.assertIn('2: making net with feather', [row.text for row in rows])
+		'''
 
+		
+		#inputbox.send_keys(Keys.ENTER)
+		self.check_row_list_table('1: buy feather')
 
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		inputbox.send_keys('making net with feather')
+		inputbox.send_keys(Keys.ENTER)
+
+		self.check_row_list_table('2: making net with feather')
+		self.check_row_list_table('1: buy feather')
 
 		self.fail('Finish the test!')
 

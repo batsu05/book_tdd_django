55029016a0877eb573e8dcccfd7eba64d80ee8b3
Brandon Jeong <batsu05@gmail.com>
2015-06-24 09:36:09 +0900

    Moved FTs into their own individual files

---
+++ functional_tests/test_list_item_validation.py
@@ -0,0 +1,11 @@
+from functional_tests.base import FunctionalTest
+from unittest import skip
+
+
+class ItemValidationTest(FunctionalTest):
+	
+	def test_cannot_add_empty_list_items(self):
+
+		self.fail('write me!')
+
+		
